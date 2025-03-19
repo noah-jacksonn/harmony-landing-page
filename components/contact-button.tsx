84fdd4f9019957339edc6a293a9920440ac93a90
Noah Jackson <njackson6100@gmail.com>
2025-03-19 12:07:52 -0400

    updated contact button to route to harmonyappai@gmail.com

--- components/contact-button.tsx
+++ components/contact-button.tsx
@@ -5,7 +5,7 @@
 export default function ContactButton() {
   return (
     <a
-      href="mailto:njackson6100@gmail.com?subject=Harmony%20-%20"
+      href="mailto:harmonyappai@gmail.com?subject=Harmony%20-%20"
       className="inline-block bg-black text-white font-medium px-8 py-4 rounded-full hover:bg-gray-800 transition-colors duration-200 text-xl"
     >
       Get In Touch
